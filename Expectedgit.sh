1c8578a9d4af4ca16a901b3bb2c07a46d96dd57a
ben <3050760440@qq.com>
2022-01-05 11:30:57 +0800

    默认提交, 提交人: Administrator, 提交时间: 2022-01-05 11:30:57

---
+++ Expectedgit.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+
+echo "GitAutoPull Start"
+git pull origin main
+echo "GitAutoPull End"
+
+echo "GitAutoPush Starting..."
+time=$(date "+%Y-%m-%d %H:%M:%S")
+git add .
+
+
+git commit -m "默认提交, 提交人: $(whoami), 提交时间: ${time}"
+
+
+	
+git push origin main
+echo "GitAutoPush End"
+
+read -t 30 -p "同步结束" msg
